a8dd673497f25cb34ed49192234d847d2621c457
machuanjia <machuanjia@worktile.com>
2017-11-22 11:35:44 +0800

    add pages

---
+++ src/pages/site/site.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="site">
+    <!--<img src="./assets/logo.png">-->sss
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'site'
+}
+</script>
+
+<style>
+
+</style>
